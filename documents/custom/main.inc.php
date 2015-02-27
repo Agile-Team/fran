6e480df0b967d616c04437672cd05de15d0d2af9
Salva Mateo <smateocu@yahoo.com>
2015-02-27 13:16:35 +0100

    BULK INITIAL COMMIT TO MASTER

---
+++ documents/custom/main.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include main into htdocs
+include_once '/Library/Server/Web/Data/Sites/dolibar.smateocu.ovh/htdocs/main.inc.php';
+?>
---
+++ documents/custom/master.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include master into htdocs
+include_once '/Library/Server/Web/Data/Sites/dolibar.smateocu.ovh/htdocs/master.inc.php';
+?>
